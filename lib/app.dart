e5a86b1522e8569861ebccb1645bc2a8fdcc7435
gashukov <mashukovg@gmail.com>
2021-05-24 16:15:38 +0700

    Revert "все красиво, но миллион запросов"
    This reverts commit fbb2a4d070b528553eafdaf4f1d967a2e7f77fab.

--- lib/app.dart
+++ lib/app.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 import 'constants.dart';
-import 'controllers/MenuController.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
 
@@ -15,36 +13,29 @@ class BricksBuildInstructionsApp extends StatelessWidget {
     var localizationDelegate = LocalizedApp.of(context).delegate;
 
     return LocalizationProvider(
-        state: LocalizationProvider.of(context).state,
-        child: MultiProvider(
-          providers: [
-            ChangeNotifierProvider(
-              create: (context) => MenuController(),
-            ),
-          ],
-          child: MaterialApp(
-            localizationsDelegates: [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              localizationDelegate
-            ],
-            supportedLocales: localizationDelegate.supportedLocales,
-            locale: localizationDelegate.currentLocale,
-            onGenerateTitle: (BuildContext context) => translate('app_title'),
-            // theme: ThemeData(primaryColor: Colors.red.shade900),
-            theme: ThemeData.dark().copyWith(
-              scaffoldBackgroundColor: bgColor,
-              textTheme:
-                  GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-                      .apply(bodyColor: Colors.white),
-              canvasColor: secondaryColor,
-            ),
-            debugShowCheckedModeBanner: false,
-            routes: {
-              '/': (context) => HomePage(),
-              // '/login': (context) => LoginPage(),
-            },
-          ),
-        ));
+      state: LocalizationProvider.of(context).state,
+      child: MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          localizationDelegate
+        ],
+        supportedLocales: localizationDelegate.supportedLocales,
+        locale: localizationDelegate.currentLocale,
+        onGenerateTitle: (BuildContext context) => translate('app_title'),
+        // theme: ThemeData(primaryColor: Colors.red.shade900),
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: bgColor,
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+              .apply(bodyColor: Colors.white),
+          canvasColor: secondaryColor,
+        ),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => HomePage(),
+          // '/login': (context) => LoginPage(),
+        },
+      ),
+    );
   }
 }
